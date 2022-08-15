ff9c354b40a67f8fd5fe3f8a71409529cfe320e1
Mark Akom <mark.akom2019@gmail.com>
2022-08-15 09:54:35 +0000

    Data file to hold information about the cards

---
+++ src/data.js
@@ -0,0 +1,42 @@
+import Sad from './images/sad.svg';
+import Cool from './images/cool.svg';
+import Wink from './images/wink.svg';
+import Star from './images/star.svg';
+import Smile from './images/smile.svg';
+import Cry from './images/cry.svg';
+
+
+const list = [
+    {
+        id: 1,
+        title: "Sad",
+        url: Sad
+    },
+    {
+        id: 2,
+        title: "Cool",
+        url: Cool
+    },
+    {
+        id: 3,
+        title: "Wink",
+        url: Wink
+    },
+    {
+        id: 4,
+        title: "Star",
+        url: Star
+    },
+    {
+        id: 5,
+        title: "Smile",
+        url: Smile
+    },
+    {
+        id: 6,
+        title: "Cry",
+        url: Cry
+    },
+]
+
+export default list
