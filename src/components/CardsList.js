6adde4cf599f2364b1ff14957d4058e5edac5222
Mark Akom <mark.akom2019@gmail.com>
2022-08-15 15:14:29 +0000

    completed cardlist logic whic imports and renders cards

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,6 +1,15 @@
+import Card from './Card'
 
-const CardsList = () => {
-  return 
+const CardsList = ({ listItems, updateScores }) => {
+  return (
+    <section className="cards-list">
+      {
+        listItems.map(cardData => {
+          return <Card cardData={cardData} key={cardData.id} updateScores={updateScores} />
+        })
+      }
+      </section>
+  )
 }
 
 export default CardsList
